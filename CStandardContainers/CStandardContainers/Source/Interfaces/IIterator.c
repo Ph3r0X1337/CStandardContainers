d98e3eb41a3c2aadd53e8ada5270e7109d8f1b17
Ph3r0X <121265624+Ph3r0X1337@users.noreply.github.com>
2025-12-25 15:46:56 +0100

    Added IIterator interface and fixed an error in the Windows configuration.

---
+++ CStandardContainers/CStandardContainers/Source/Interfaces/IIterator.c
@@ -0,0 +1,62 @@
+#include "IIterator.h"
+
+CSC_STATUS CSCMETHOD CSC_IIteratorInvalidateIteration(_Inout_ struct _CSC_IIterator* CONST pThis)
+{
+	if (!pThis || !pThis->pIIteratorVirtualTable || !pThis->pIIteratorVirtualTable->pInvalidateIteration)
+	{
+		return CSC_STATUS_INVALID_PARAMETER;
+	}
+	else
+	{
+		return pThis->pIIteratorVirtualTable->pInvalidateIteration(pThis);
+	}
+}
+
+CSC_STATUS CSCMETHOD CSC_IIteratorUpdateIteration(_Inout_ struct _CSC_IIterator* CONST pThis)
+{
+	if (!pThis || !pThis->pIIteratorVirtualTable || !pThis->pIIteratorVirtualTable->pUpdateIteration)
+	{
+		return CSC_STATUS_INVALID_PARAMETER;
+	}
+	else
+	{
+		return pThis->pIIteratorVirtualTable->pUpdateIteration(pThis);
+	}
+}
+
+
+CSC_STATUS CSCMETHOD CSC_IIteratorOnInsertion(_Inout_ struct _CSC_IIterator* CONST pThis, CONST CSC_SIZE_T insertIndex, CONST CSC_SIZE_T numOfElements, CONST CSC_SIZE_T newSize)
+{
+	if (!numOfElements || insertIndex + numOfElements > newSize || !pThis || !pThis->pIIteratorVirtualTable || !pThis->pIIteratorVirtualTable->pOnInsertion)
+	{
+		return CSC_STATUS_INVALID_PARAMETER;
+	}
+	else
+	{
+		return pThis->pIIteratorVirtualTable->pOnInsertion(pThis, insertIndex, numOfElements, newSize);
+	}
+}
+
+CSC_STATUS CSCMETHOD CSC_IIteratorOnRemoval(_Inout_ struct _CSC_IIterator* CONST pThis, CONST CSC_SIZE_T removeIndex, CONST CSC_SIZE_T numOfElements, CONST CSC_SIZE_T newSize)
+{
+	if (!numOfElements || removeIndex > newSize || !pThis || !pThis->pIIteratorVirtualTable || !pThis->pIIteratorVirtualTable->pOnRemoval)
+	{
+		return CSC_STATUS_INVALID_PARAMETER;
+	}
+	else
+	{
+		return pThis->pIIteratorVirtualTable->pOnRemoval(pThis, removeIndex, numOfElements, newSize);
+	}
+}
+
+CSC_STATUS CSCMETHOD CSC_IIteratorOnDestruction(_Inout_ struct _CSC_IIterator* CONST pThis)
+{
+	if (!pThis || !pThis->pIIteratorVirtualTable || !pThis->pIIteratorVirtualTable->pOnDestruction)
+	{
+		return CSC_STATUS_INVALID_PARAMETER;
+	}
+	else
+	{
+		return pThis->pIIteratorVirtualTable->pOnDestruction(pThis);
+	}
+}
---
+++ CStandardContainers/CStandardContainers/Source/Interfaces/IIterator.h
@@ -0,0 +1,61 @@
+#ifndef CSC_I_ITERATOR
+#define CSC_I_ITERATOR
+
+/*
+Description:
+The IIterator interface must be implemented by all iterator objects that wish to iterate over the iterables of the CSC library.
+The iterable uses the IIterator interface as a observer pattern, to notify the subscribed iterator objects on any updates that occurr during changes in the iterable.
+While the IIterator interface methods could be directly called on the iterator by an arbitrary caller, this use is discouraged unless there is a justified need for it.
+Instead it is encouraged to invoke the methods of the object implementing the IIterator interface to make changes to the iterator.
+*/
+
+// Include the current configuration of the library containing various type definitions and other things.
+#include "../Configuration/Configuration.h"
+
+// Predecleration of the CSC_IIterator type to be able to create the function pointer types for the methods that the interface implements.
+struct _CSC_IIterator;
+
+// Type definitions of the function pointer types of the IIterator interfaces methods, which need to be implemented by types that utilize the interface.
+typedef CSC_STATUS(CSCMETHOD* CSC_P_I_ITERATOR_INVALIDATE_ITERATION)(_Inout_ struct _CSC_IIterator* CONST pThis);
+typedef CSC_STATUS(CSCMETHOD* CSC_P_I_ITERATOR_UPDATE_ITERATION)(_Inout_ struct _CSC_IIterator* CONST pThis);
+
+typedef CSC_STATUS(CSCMETHOD* CSC_P_I_ITERATOR_ON_INSERTION)(_Inout_ struct _CSC_IIterator* CONST pThis, CONST CSC_SIZE_T insertIndex, CONST CSC_SIZE_T numOfElements, CONST CSC_SIZE_T newSize);
+typedef CSC_STATUS(CSCMETHOD* CSC_P_I_ITERATOR_ON_REMOVAL)(_Inout_ struct _CSC_IIterator* CONST pThis, CONST CSC_SIZE_T removeIndex, CONST CSC_SIZE_T numOfElements, CONST CSC_SIZE_T newSize);
+typedef CSC_STATUS(CSCMETHOD* CSC_P_I_ITERATOR_ON_DESTRUCTION)(_Inout_ struct _CSC_IIterator* CONST pThis);
+
+// Definition of the virtual table layout of the IIterator type.
+typedef struct _CSC_IIteratorVirtualTable
+{
+	CSC_P_I_ITERATOR_INVALIDATE_ITERATION pInvalidateIteration;
+	CSC_P_I_ITERATOR_UPDATE_ITERATION pUpdateIteration;
+	CSC_P_I_ITERATOR_ON_INSERTION pOnInsertion;
+	CSC_P_I_ITERATOR_ON_REMOVAL pOnRemoval;
+	CSC_P_I_ITERATOR_ON_DESTRUCTION pOnDestruction;
+} CSC_IIteratorVirtualTable;
+
+// Definition of the IIterator type, contains a pointer to the virtual table of the object that implements the interfaces methods.
+typedef struct _CSC_IIterator
+{
+	CSC_IIteratorVirtualTable* pIIteratorVirtualTable;
+} CSC_IIterator;
+
+// Calls the underlying method to invalidate the iteration, if it is currently valid.
+// Should return CSC_STATUS_SUCCESS on success, otherwise an error code is returned.
+CSC_STATUS CSCMETHOD CSC_IIteratorInvalidateIteration(_Inout_ struct _CSC_IIterator* CONST pThis);
+// Calls the underlying method to update the iteration, if it is currently valid.
+// This method is usually called when the iterables internal resources have changed, which results in a need to update pointers to elements within the iterable.
+// Should return CSC_STATUS_SUCCESS on success, otherwise an error code is returned.
+CSC_STATUS CSCMETHOD CSC_IIteratorUpdateIteration(_Inout_ struct _CSC_IIterator* CONST pThis);
+
+// Calls the underlying method to notify the iterator on a insertion of elements in the iterable, which is typical when the iterable is also a container.
+// Should return CSC_STATUS_SUCCESS on success, otherwise an error code is returned.
+CSC_STATUS CSCMETHOD CSC_IIteratorOnInsertion(_Inout_ struct _CSC_IIterator* CONST pThis, CONST CSC_SIZE_T insertIndex, CONST CSC_SIZE_T numOfElements, CONST CSC_SIZE_T newSize);
+// Calls the underlying method to notify the iterator on a removal of elements in the iterable, which is typical when the iterable is also a container.
+// Should return CSC_STATUS_SUCCESS on success, otherwise an error code is returned.
+CSC_STATUS CSCMETHOD CSC_IIteratorOnRemoval(_Inout_ struct _CSC_IIterator* CONST pThis, CONST CSC_SIZE_T removeIndex, CONST CSC_SIZE_T numOfElements, CONST CSC_SIZE_T newSize);
+// Calls the underlying method to indicate the destruction of the iterable object.
+// The iterable object should unregister the bound iterator itself, hence this method should not invoke unregistration on the iterable.
+// Should return CSC_STATUS_SUCCESS on success, otherwise an error code is returned.
+CSC_STATUS CSCMETHOD CSC_IIteratorOnDestruction(_Inout_ struct _CSC_IIterator* CONST pThis);
+
+#endif CSC_I_ITERATOR
